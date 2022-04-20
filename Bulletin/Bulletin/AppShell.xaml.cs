5d5ac102cca46cc6643962edb92356674219dbb3
mjn021 <103533801+mjn021@users.noreply.github.com>
2022-04-20 14:06:33 -0400

    Add project files.

---
+++ Bulletin/Bulletin/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Bulletin.ViewModels;
+using Bulletin.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Bulletin
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ Bulletin/Bulletin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Bulletin.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Bulletin.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
